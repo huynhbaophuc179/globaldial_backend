ecd210d1cdcf25959d29eab44eaf61d263ef3e5b
Phuchb <huynhbaophuc179a@gmail.com>
2023-05-18 21:02:30 +0700

    added the barebone of the connection queue

--- websocket/socketController.js
+++ websocket/socketController.js
@@ -1,10 +1,9 @@
-const uuid = require('uuid');
-
 
 module.exports = (io, socket) => {
     const emailToSocketIdMap = new Map();
     const socketidToEmailMap = new Map();
     //queue for connection queuing
+    //queue stores socketID
     const connectionQueue = [];
 
     const logfunc = () => {
@@ -12,18 +11,41 @@ module.exports = (io, socket) => {
 
     }
     io.on("connection", (socket) => {
+        console.log(connectionQueue);
         console.log(`Socket Connected`, socket.id);
 
+        //logic for queuing connections
+        const matchConnection = () => {
+            if (connectionQueue.length > 1) {
+                // socket id of the two first socket of the queue
+                cnn1 = connectionQueue.shift()
+                cnn2 = connectionQueue.shift()
+                if (cnn1 && cnn2) {
+                    console.log("matched" + socketidToEmailMap.get(cnn1));
+                    console.log("matched" + socketidToEmailMap.get(cnn2));
+                } else {
+                    console.log("some thing gone wrong");
+                }
 
+            }
+            else {
+                console.log("not enough people");
 
+            }
+        }
+
+        // socket.on("room:enqueue", (data) => {
+        //     connectionQueue.push(socket.id);
+        // })
         socket.on("room:join", (data) => {
-            const { email, } = data;
+            const { email, room } = data;
             emailToSocketIdMap.set(email, socket.id);
             socketidToEmailMap.set(socket.id, email);
             io.to(room).emit("user:joined", { email, id: socket.id });
             socket.join(room);
             io.to(socket.id).emit("room:join", data);
-
+            connectionQueue.push(socket.id);
+            matchConnection()
         });
 
         socket.on("user:call", ({ to, offer }) => {
