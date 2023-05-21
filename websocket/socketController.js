710d3423cacb500ecb18d29dc848fff4c9624865
Phuchb <huynhbaophuc179a@gmail.com>
2023-05-21 15:17:06 +0700

    staged changes before mergin two repo

--- websocket/socketController.js
+++ websocket/socketController.js
@@ -16,9 +16,9 @@ module.exports = (io, socket) => {
             if (!this.set.has(item)) {
                 this.set.add(item);
                 this.array.push(item);
-                console.log(`Item ${item} added to the unique queue.`);
+
             } else {
-                console.log(`Item ${item} already exists in the unique queue.`);
+
             }
         },
 
@@ -29,7 +29,7 @@ module.exports = (io, socket) => {
                 console.log(`Item ${item} removed from the unique queue.`);
                 return item;
             } else {
-                console.log('The unique queue is empty.');
+
                 return undefined;
             }
         },
@@ -37,7 +37,7 @@ module.exports = (io, socket) => {
         clear() {
             this.set.clear();
             this.array.length = 0;
-            console.log('The unique queue has been cleared.');
+
         },
 
         size() {
@@ -49,31 +49,54 @@ module.exports = (io, socket) => {
         console.log(connectionQueue.array);
         console.log(`Socket Connected`, socket.id);
 
+
+        //logic to handle disconection 
+        socket.on("disconnect", () => {
+            const email = socketidToEmailMap.get(socket.id)
+            emailToSocketIdMap.delete(email)
+            socketidToEmailMap.delete(socket.id)
+            console.log(socketidToEmailMap);
+            console.log(emailToSocketIdMap);
+            console.log(email, " is the mail");
+            console.log("A user disconnected");
+
+        });
+
         //logic for queuing connections
         const matchConnection = (email) => {
 
             if (connectionQueue.size() > 1) {
                 // socket id of the two first socket of the queue
-                cnn1 = connectionQueue.dequeue()
-                cnn2 = connectionQueue.dequeue()
-                console.log(cnn1, "+", cnn2);
-                if (cnn1 && cnn2) {
-                    console.log("matched" + socketidToEmailMap.get(cnn1));
-                    console.log("matched" + socketidToEmailMap.get(cnn2));
+                socket_id_1 = connectionQueue.dequeue()
+                socket_id_2 = connectionQueue.dequeue()
+                const isSocketConnected1 = io.sockets.sockets.has(socket_id_1);
+                const isSocketConnected2 = io.sockets.sockets.has(socket_id_2);
+                console.log(socket_id_1, "+", socket_id_2);
+                if (isSocketConnected1 && isSocketConnected2) {
+                    console.log("matched" + socketidToEmailMap.get(socket_id_1));
+                    console.log("matched" + socketidToEmailMap.get(socket_id_2));
                     const roomId = uuidv4();
                     roomIdToSocketMap.set(roomId, socket.id);
-                    const newData1 = { email: socketidToEmailMap.get(cnn1), room: roomId }
-                    const newData2 = { email: socketidToEmailMap.get(cnn2), room: roomId }
+                    const newData1 = { email: socketidToEmailMap.get(socket_id_1), room: roomId }
+                    const newData2 = { email: socketidToEmailMap.get(socket_id_2), room: roomId }
 
                     socket.join(roomId);
-                    io.to(cnn2).emit("room:join", newData2);
-                    io.to(cnn1).emit("room:join", newData1);
-                    io.to(cnn1).emit("user:joined", { email: socketidToEmailMap.get(cnn2), id: cnn2 });
-                    io.to(cnn2).emit("user:joined", { email: socketidToEmailMap.get(cnn1), id: cnn1 });
+                    io.to(socket_id_2).emit("room:join", newData2);
+                    io.to(socket_id_1).emit("room:join", newData1);
+                    io.to(socket_id_1).emit("user:joined", { email: socketidToEmailMap.get(socket_id_2), id: socket_id_2 });
+                    io.to(socket_id_2).emit("user:joined", { email: socketidToEmailMap.get(socket_id_1), id: socket_id_1 });
 
                 } else {
-                    console.log("both");
-                    connectionQueue.enqueue(cnn1)
+
+                    if (isSocketConnected1) {
+                        console.log("fall back!, enqueued socket 1");
+                        connectionQueue.enqueue(socket_id_1)
+                    }
+                    else {
+                        console.log("fall back!, enqueued socket 2");
+                        connectionQueue.enqueue(socket_id_2)
+                    }
+
                 }
 
             }
@@ -83,26 +106,17 @@ module.exports = (io, socket) => {
         //     connectionQueue.push(socket.id);
         // })
         socket.on("room:join", (data) => {
-
             //create a unique room name
-
             const { email, room } = data;
             // map for tracking reasons
             emailToSocketIdMap.set(email, socket.id);
             socketidToEmailMap.set(socket.id, email);
-
-
             // used the old logic to create new room and send its data
             // to preserve the old logic in the frontend side
-
             //push in to the queue first to ensure the conncurency
-
             connectionQueue.enqueue(socket.id);
             console.log(connectionQueue.array);
             matchConnection(email);
-
-
-
         });
 
         socket.on("user:call", ({ to, offer }) => {
