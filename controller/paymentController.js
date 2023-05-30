de861f30afff7656ed9c99e016706f5b39289a41
Phuchb <huynhbaophuc179a@gmail.com>
2023-05-30 22:53:20 +0700

    foundation for loging and tracking user subscription

---
+++ controller/paymentController.js
@@ -0,0 +1,40 @@
+
+// base URL will need to change for production applications
+const base = "https://api-m.sandbox.paypal.com";
+
+const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env; // pull from environment variables
+
+// base URL will need to change for production applications
+const baseURL = {
+    sandbox: "https://api-m.sandbox.paypal.com",
+    production: "https://api-m.paypal.com"
+};
+
+// call this function to create your client token
+async function generateClientToken() {
+    const accessToken = await generateAccessToken();
+    const response = await fetch(`${baseURL.sandbox}/v1/identity/generate-token`, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Accept-Language": "en_US",
+            "Content-Type": "application/json",
+        },
+    });
+    const data = await response.json();
+    return data.client_token;
+}
+
+// access token is used to authenticate all REST API requests
+async function generateAccessToken() {
+    const auth = Buffer.from(CLIENT_ID + ":" + APP_SECRET).toString("base64");
+    const response = await fetch(`${baseURL.sandbox}/v1/oauth2/token`, {
+        method: "POST",
+        body: "grant_type=client_credentials",
+        headers: {
+            Authorization: `Basic ${auth}`,
+        },
+    });
+    const data = await response.json();
+    return data.access_token;
+}
--- routes/login.js
+++ routes/login.js
@@ -1,52 +1,85 @@
-var express = require('express');
-var router = express.Router();
-var User = require("../schema/user");
-/* GET users listing. */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-router.post('/', async function (req, res, next) {
-    try {
-        const { uidFirebase } = req.body
-        console.log(uidFirebase);
-        const UserRecord = await req.auth.getUser(uidFirebase)
-        console.log("Logged in");
-        const userMongoData = await User.findOne({ uidFirebase: UserRecord.uid })
-        console.log(userMongoData);
-        if (userMongoData) {
-            console.log("old user detected");
-            return res.json({
-                status: 200,
-                message: "Success",
-                data: userMongoData[0]
+function App() {
+    const FUNDING_SOURCES = [
+        paypal.FUNDING.PAYPAL,
+        paypal.FUNDING.CARD
+    ];
 
-            })
-        } else {
-            console.log("create new user");
-            const newUser = new User({
-                username: UserRecord.email.split("@")[0],
-                displayName: UserRecord.displayName,
-                email: UserRecord.email,
-                uidFirebase: UserRecord.uid,
-                photoUrl: UserRecord.photoUrl,
-                phoneNumber: UserRecord.phoneNumber
-            })
-            const save = await newUser.save()
-            console.log(save);
-            return res.json({
-                status: 200,
-                message: "Success",
-                data: UserRecord
+    return (
+        <div className="App">
+            <PayPalScriptProvider options={{ "client-id": "<test>" }}>
+                {
+                    FUNDING_SOURCES.map(fundingSource => {
+                        <PayPalButtons
+                            fundingSource=fundingSource
 
-            })
+                            style={{
+                                layout: 'vertical',
+                                shape: 'rect',
+                                color: (fundingSource == paypal.FUNDING.PAYLATER) ? 'gold' : '',
+                            }}
+
+                            createOrder={async (data, actions) => {
+                                const response = await fetch("http://localhost:9597/orders", {
+                                    method: "POST"
+                                });
+                                const details = await response.json();
+                                return details.id;
+                            }}
+
+                            createSubscription={async (data, actions) => {
+                                return actions.subscription.create({
+                                    plan_id: "P-3RX065706M3469222L5IFM4I",
+                                });
+                            };
+
+                        onApprove = { async(data, actions) => {
+                const response = await fetch(`http://localhost:9597/orders/${data.orderID}/capture`, {
+                    method: "POST"
+                });
+                const details = await response.json();
+                // Three cases to handle:
+                //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                //   (2) Other non-recoverable errors -> Show a failure message
+                //   (3) Successful transaction -> Show confirmation or thank you
+
+                // This example reads a v2/checkout/orders capture response, propagated from the server
+                // You could use a different API or structure for your 'orderData'
+
+                const errorDetail = Array.isArray(details.details) && details.details[0];
+                if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
+                  return actions.restart(); // Recoverable state, per:
+                  // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
+                }
+
+                if (errorDetail) {
+                    let msg = 'Sorry, your transaction could not be processed.';
+                if (errorDetail.description) msg += '' + errorDetail.description;
+                if (details.debug_id) msg += ' (' + details.debug_id + ')';
+                return alert(msg); // Show a failure message (try to avoid alerts in production environments)
+                }
+
+                // Successful capture! For demo purposes:
+                console.log('Capture result', details, JSON.stringify(details, null, 2));
+                const transaction = details.purchase_units[0].payments.captures[0];
+                alert('Transaction '+ transaction.status + ': ' + transaction.id + 'See console for all available details');
+              }}
+            />
+          })
         }
-    }
-    catch (error) {
-        console.log('Error fetching user data:', error);
-        res.json({
-            status: 500,
-            message: "Internal Server Error",
-            data: []
-        })
-    };
-});
+            </PayPalScriptProvider>
+        </div>
+    );
+}
 
-module.exports = router;
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
